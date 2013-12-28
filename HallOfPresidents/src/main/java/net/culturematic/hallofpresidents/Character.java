79472a70539ccefde8fbd772c5647b58c5d651dc
Joe Bowers <joerbowers@gmail.com>
2013-12-27 17:29:38 -0800

    IT WALKS!

--- HallOfPresidents/src/main/java/net/culturematic/hallofpresidents/Character.java
+++ HallOfPresidents/src/main/java/net/culturematic/hallofpresidents/Character.java
@@ -14,17 +14,20 @@ public class Character {
     // ( x, 1), (x, 2), (x, 3) -> Walk cycles for facing X
     public Character(Bitmap spritesheet, AssetLoader loader, PointF startPosition) {
         mSpriteSheet = spritesheet;
-        int spriteSize = loader.scaleInt(128);
+        mSpriteSize = loader.scaleInt(128);
+        mAnimationFrameDistance = loader.scaleInt(128 / 4);
 
         // TODO this rect needs to be sensitive to bitmap scale,
         // and to be chosen based on motion direction.
-        mSourceRect = new Rect(0, 0, spriteSize, spriteSize);
+        mSourceRect = new Rect(0, 0, mSpriteSize, mSpriteSize);
 
         // TODO this rect needs to be centered almost all of the time.
         // There should probably be a viewport abstraction that deals with this position.
-        mDestRect = new Rect(0, 0, spriteSize, spriteSize);
+        mDestRect = new Rect(0, 0, mSpriteSize, mSpriteSize);
 
         mPosition = startPosition; // TODO This should be in GameState.
+        mAnimationDirection = UIControls.Direction.DIRECTION_DOWN;
+        mAnimationDistance = 0;
         mLastTime = -1;
         mSpeed = 1f/10000000f; // pixels per nano?
     }
@@ -46,7 +49,6 @@ public class Character {
                 break;
             case DIRECTION_RIGHT:
                 mPosition.x += distance;
-                Log.d(LOGTAG, "MOVE dTime " + deltaTime + ", distance " + distance + ", position x " + mPosition.x);
                 break;
             case DIRECTION_LEFT:
                 mPosition.x -= distance;
@@ -56,21 +58,57 @@ public class Character {
         }
 
         mLastTime = nanoTime;
+
+        if (UIControls.Direction.DIRECTION_NONE == direction) {
+            mAnimationDistance = 0;
+        } else if (mAnimationDirection != direction) {
+            mAnimationDirection = direction;
+            mAnimationDistance = distance % (mAnimationFrameDistance * ANIMATION_LENGTH_IN_FRAMES);
+        } else {
+            mAnimationDistance =
+                    (mAnimationDistance + distance) % (mAnimationFrameDistance * ANIMATION_LENGTH_IN_FRAMES);
+
+        }
     }
 
     public void drawCharacter(Canvas canvas) {
+        int frame = (int) mAnimationDistance / mAnimationFrameDistance;
+        int frameOffset = frame * mSpriteSize;
+        int xOffset = 0;
+        switch (mAnimationDirection) {
+            case DIRECTION_NONE:
+                throw new RuntimeException("Animation direction should never be NONE");
+            case DIRECTION_DOWN:
+                xOffset = 0;
+                break;
+            case DIRECTION_UP:
+                xOffset = mSpriteSize;
+                break;
+            case DIRECTION_LEFT:
+                xOffset = mSpriteSize * 2;
+                break;
+            case DIRECTION_RIGHT:
+                xOffset = mSpriteSize * 3;
+                break;
+        }
+        mSourceRect.set(xOffset, frameOffset, xOffset + mSpriteSize, frameOffset + mSpriteSize);
+
         mDestRect.offsetTo((int) mPosition.x, (int) mPosition.y);
         canvas.drawBitmap(mSpriteSheet, mSourceRect, mDestRect, null);
     }
 
     private long mLastTime;
+    private UIControls.Direction mAnimationDirection;
+    private float mAnimationDistance;
 
-
+    private final int mSpriteSize;
+    private final int mAnimationFrameDistance;
     private final PointF mPosition;
     private final float mSpeed;
     private final Bitmap mSpriteSheet;
     private final Rect mSourceRect;
     private final Rect mDestRect;
 
+    private static final int ANIMATION_LENGTH_IN_FRAMES = 4;
     private static final String LOGTAG = "hallofpresidents.Character";
 }
